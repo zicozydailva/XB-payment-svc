62408586a434e9bc1ea027fe0a84bb68129307ce
zicozydailva <zicozydasilva70@gmail.com>
2024-05-29 19:37:38 +0100

    fixes: svc clean-up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './modules/user/entities/user.entity';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Ensures ConfigModule is available globally
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Ogbu10ski',
+      database: 'payment-svc',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
