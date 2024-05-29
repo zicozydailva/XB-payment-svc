77c8b8aa293df9f3531318cacd70692a4b82288b
zicozydailva <zicozydasilva70@gmail.com>
2024-05-29 20:14:28 +0100

    chore: user created:event successfully synced

---
+++ src/constants/events.constant.ts
@@ -0,0 +1,3 @@
+export const SEND_MAIL = 'SEND_MAIL';
+export const USER_CREATED = 'USER_CREATED';
+export const USER_UPDATED = 'USER_UPDATED';
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,20 +1,21 @@
 import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { EventPattern, Payload } from '@nestjs/microservices';
+import { USER_CREATED, USER_UPDATED } from 'src/constants';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @UsePipes(new ValidationPipe())
-  @EventPattern('USER_CREATED')
+  @EventPattern(USER_CREATED)
   create(@Payload() createUserDto: any) {
     console.log(createUserDto);
     return this.userService.create(createUserDto);
   }
 
   @UsePipes(new ValidationPipe())
-  @EventPattern('USER_DELETED')
+  @EventPattern(USER_UPDATED)
   delete(@Payload() deleteUserDto: any) {
     console.log(deleteUserDto);
     return this.userService.delete(deleteUserDto);
